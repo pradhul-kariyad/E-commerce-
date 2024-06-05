89ef9c46ae387926d917d4803e22f9020b5f8bc1
Pradhul <pradhulkariyad@gmail.com>
2024-06-05 22:33:47 +0530

    29

--- lib/View/widgets/ipaddress/ipaddress.dart
+++ lib/View/widgets/ipaddress/ipaddress.dart
@@ -1,3 +1,3 @@
 // ignore_for_file: file_names
 
-dynamic ip = '192.168.1.19';
+dynamic ip = '192.168.134.47';
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,6 @@ import Foundation
 
 import awesome_notifications
 import file_selector_macos
-import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import smart_auth
@@ -15,7 +14,6 @@ import smart_auth
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
